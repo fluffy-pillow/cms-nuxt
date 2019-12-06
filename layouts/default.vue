e69db5f431737c496a259d64c4bd033a52921d34
fluffy-pillow <igornebudet@bk.ru>
2019-12-06 20:37:50 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="kt-grid kt-grid--ver kt-grid--root kt-page">
+    <nuxt />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v3 kt-login--signin" id="kt_login">
+    <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor"
+         :style="{backgroundImage: 'url(' + require('@/assets/media/bg/bg.jpg') + ')'}">
+      <div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
+        <div class="kt-login__container">
+          <div class="kt-login__logo">
+            <a href="#">
+              <img src="../assets/media/logos/logo.png">
+            </a>
+          </div>
+          <div class="kt-login__signin">
+            <div class="kt-login__head">
+              <h3 class="kt-login__title">Sign In To Admin</h3>
+            </div>
+            <form class="kt-form" action="" @submit.prevent="onSubmit">
+              <div class="input-group">
+                <input class="form-control"
+                       type="text"
+                       placeholder="Email"
+                       name="email"
+                       autocomplete="off"
+                       v-validate="'required|email'"
+                >
+                <div class="error invalid-feedback" :class="{show: errors.first('email')}">
+                  {{ errors.first('email') }}
+                </div>
+              </div>
+              <div class="input-group">
+                <input class="form-control"
+                       type="password"
+                       placeholder="Password"
+                       name="password"
+                       v-validate="'required|password'"
+                >
+                <div class="error invalid-feedback" :class="{show: errors.first('email')}">
+                  {{ errors.first('password') }}
+                </div>
+              </div>
+              <div class="row kt-login__extra">
+                <div class="col">
+                  <label class="kt-checkbox">
+                    <input type="checkbox" name="remember"> Remember me
+                    <span></span>
+                  </label>
+                </div>
+              </div>
+              <div class="kt-login__actions">
+                <button id="kt_login_signin_submit" class="btn btn-brand btn-elevate kt-login__btn-primary">Sign In</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: "login",
+        data () {
+          return {
+            email: '',
+            password: ''
+          }
+        },
+        methods: {
+          onSubmit (e) {
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
