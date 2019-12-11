b4e2bd964dc9236fec8f0540f48cfae7e0f8e1c7
fluffy-pillow <igornebudet@bk.ru>
2019-12-11 20:23:15 +0300

    дэшборд: юзербар (десктоп)

--- components/topbar/userBar.vue
+++ components/topbar/userBar.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="kt-header__topbar-item kt-header__topbar-item--user">
-    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px">
+  <div class="kt-header__topbar-item kt-header__topbar-item--user"
+       :class="{show: bOpen}"
+       v-click-outside="close"
+  >
+  <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px" ref="topbarWrapper">
       <span class="kt-hidden kt-header__topbar-welcome">Hi,</span>
       <span class="kt-hidden kt-header__topbar-username">Nick</span>
-      <img class="kt-hidden" alt="Pic" src="assets/media/users/300_21.jpg" />
-      <span class="kt-header__topbar-icon"><i class="flaticon2-user-outline-symbol"></i></span>
+      <img class="kt-hidden" alt="Pic" src="../../assets/media/users/300_21.jpg" />
+      <span class="kt-header__topbar-icon" @click="toggle">
+        <i class="flaticon2-user-outline-symbol"></i>
+      </span>
     </div>
-    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl">
+    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl"
+         :class="{show: bOpen}"
+         :style="{transform: 'translate3d(' + dropdownFitPosition + 'px, 0, 0)'}"
+    >
 
       <!--begin: Head -->
       <div class="kt-user-card kt-user-card--skin-light kt-notification-item-padding-x">
         <div class="kt-user-card__avatar">
-          <img class="kt-hidden-" alt="Pic" src="assets/media/users/300_25.jpg" />
+          <img class="kt-hidden-" alt="Pic" src="../../assets/media/users/300_25.jpg" />
 
           <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
           <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold kt-hidden">S</span>
@@ -28,71 +36,27 @@
 
       <!--begin: Navigation -->
       <div class="kt-notification">
-        <a href="custom/apps/user/profile-1/personal-information.html" class="kt-notification__item">
+        <nuxt-link to="" class="kt-notification__item"
+             v-for="(item, key) of items"
+             :key="key"
+        >
           <div class="kt-notification__item-icon">
-            <i class="flaticon2-calendar-3 kt-font-success"></i>
+            <i :class="item.iconClass"></i>
           </div>
           <div class="kt-notification__item-details">
             <div class="kt-notification__item-title kt-font-bold">
-              My Profile
+              {{item.title}}
             </div>
             <div class="kt-notification__item-time">
-              Account settings and more
+              {{item.desc}}
+              <span class="kt-badge kt-badge--danger kt-badge--inline kt-badge--pill kt-badge--rounded" v-if="item.notificationsCount">
+              {{item.notificationsCount}} pending
+            </span>
             </div>
           </div>
-        </a>
-        <a href="custom/apps/user/profile-3.html" class="kt-notification__item">
-          <div class="kt-notification__item-icon">
-            <i class="flaticon2-mail kt-font-warning"></i>
-          </div>
-          <div class="kt-notification__item-details">
-            <div class="kt-notification__item-title kt-font-bold">
-              My Messages
-            </div>
-            <div class="kt-notification__item-time">
-              Inbox and tasks
-            </div>
-          </div>
-        </a>
-        <a href="custom/apps/user/profile-2.html" class="kt-notification__item">
-          <div class="kt-notification__item-icon">
-            <i class="flaticon2-rocket-1 kt-font-danger"></i>
-          </div>
-          <div class="kt-notification__item-details">
-            <div class="kt-notification__item-title kt-font-bold">
-              My Activities
-            </div>
-            <div class="kt-notification__item-time">
-              Logs and notifications
-            </div>
-          </div>
-        </a>
-        <a href="custom/apps/user/profile-3.html" class="kt-notification__item">
-          <div class="kt-notification__item-icon">
-            <i class="flaticon2-hourglass kt-font-brand"></i>
-          </div>
-          <div class="kt-notification__item-details">
-            <div class="kt-notification__item-title kt-font-bold">
-              My Tasks
-            </div>
-            <div class="kt-notification__item-time">
-              latest tasks and projects
-            </div>
-          </div>
-        </a>
-        <a href="custom/apps/user/profile-1/overview.html" class="kt-notification__item">
-          <div class="kt-notification__item-icon">
-            <i class="flaticon2-cardiogram kt-font-warning"></i>
-          </div>
-          <div class="kt-notification__item-details">
-            <div class="kt-notification__item-title kt-font-bold">
-              Billing
-            </div>
-            <div class="kt-notification__item-time">
-              billing & statements <span class="kt-badge kt-badge--danger kt-badge--inline kt-badge--pill kt-badge--rounded">2 pending</span>
-            </div>
-          </div>
-        </a>
+        </nuxt-link>
+
+
         <div class="kt-notification__custom kt-space-between">
           <a href="custom/user/login-v2.html" target="_blank" class="btn btn-label btn-label-brand btn-sm btn-bold">Sign Out</a>
           <a href="custom/user/login-v2.html" target="_blank" class="btn btn-clean btn-sm btn-bold">Upgrade Plan</a>
@@ -106,8 +70,65 @@
 </template>
 
 <script>
+    import ClickOutside from 'vue-click-outside'
     export default {
-        name: "userBar"
+        name: "userBar",
+        data () {
+          return {
+            bOpen: false,
+            dropdownFitPosition: 0,
+            items: [
+              {
+                iconClass: 'flaticon2-calendar-3 kt-font-success',
+                title: 'My Profile',
+                desc: 'Account settings and more'
+              },
+              {
+                iconClass: 'flaticon2-mail kt-font-warning',
+                title: 'My Messages',
+                desc: 'Inbox and tasks'
+              },
+              {
+                iconClass: 'flaticon2-rocket-1 kt-font-danger',
+                title: 'My Activities',
+                desc: 'Logs and notifications'
+              },
+              {
+                iconClass: 'flaticon2-hourglass kt-font-brand',
+                title: 'My Tasks',
+                desc: 'Latest tasks and projects'
+              },
+              {
+                iconClass: 'flaticon2-cardiogram kt-font-warning',
+                title: 'Billing',
+                desc: 'billing & statements',
+                notificationsCount: 3
+              }
+            ]
+          }
+        },
+        methods: {
+          toggle () {
+            this.bOpen = !this.bOpen
+          },
+          close () {
+            this.bOpen = false
+          },
+          handleWindowResize () {
+            let topbarWrapperRect = this.$refs.topbarWrapper.getBoundingClientRect()
+            this.dropdownFitPosition = topbarWrapperRect.x - topbarWrapperRect.width - 405
+          }
+        },
+        directives: {
+          ClickOutside
+        },
+        mounted () {
+          this.handleWindowResize()
+          window.addEventListener("resize", this.handleWindowResize)
+        },
+        beforeDestroy () {
+          window.removeEventListener("resize", this.handleWindowResize)
+        }
     }
 </script>
 
