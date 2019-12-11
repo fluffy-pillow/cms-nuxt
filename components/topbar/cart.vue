5fbb4db3bb2ab9c3a4a148066c578a008cbac471
fluffy-pillow <igornebudet@bk.ru>
2019-12-11 17:45:02 +0300

    дэшборд: cart (десктоп)

--- components/topbar/cart.vue
+++ components/topbar/cart.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="kt-header__topbar-item dropdown">
-    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px">
-      <span class="kt-header__topbar-icon kt-header__topbar-icon--brand"><i class="flaticon2-shopping-cart-1"></i></span>
+  <div class="kt-header__topbar-item dropdown"
+       :class="{show: bOpen}"
+       v-click-outside="close"
+  >
+    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px"  ref="topbarWrapper">
+      <span class="kt-header__topbar-icon kt-header__topbar-icon--brand" @click="toggle">
+        <i class="flaticon2-shopping-cart-1"></i>
+      </span>
     </div>
-    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl">
+    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl"
+       :class="{show: bOpen}"
+       :style="{transform: 'translate3d(' + dropdownFitPosition + 'px, 0, 0)'}"
+       ref="dropdownMenuFit"
+    >
       <form>
-
-        <!-- begin:: Mycart -->
         <div class="kt-mycart">
-          <div class="kt-mycart__head kt-head" style="background-image: url(assets/media/misc/bg-1.jpg);">
+          <div class="kt-mycart__head kt-head"
+               :style="{backgroundImage: 'url(' + require('@/assets/media/misc/bg-1.jpg') + ')' }"
+          >
             <div class="kt-mycart__info">
               <span class="kt-mycart__icon"><i class="flaticon2-shopping-cart-1 kt-font-success"></i></span>
               <h3 class="kt-mycart__title">My Cart</h3>
@@ -17,96 +26,43 @@
               <button type="button" class="btn btn-success btn-sm" style=" ">2 Items</button>
             </div>
           </div>
-          <div class="kt-mycart__body kt-scroll" data-scroll="true" data-height="245" data-mobile-height="200">
-            <div class="kt-mycart__item">
+
+          <perfect-scrollbar class="kt-mycart__body kt-scroll ps ps--active-y"
+               data-scroll="true"
+               data-height="245"
+               data-mobile-height="200"
+               style="overflow: hidden;"
+               ref="scrollContent"
+          >
+            <div class="kt-mycart__item"
+                 v-for="(item, key) of items"
+                 :key="key"
+            >
               <div class="kt-mycart__container">
                 <div class="kt-mycart__info">
-                  <a href="#" class="kt-mycart__title">
-                    Samsung
-                  </a>
+                  <nuxt-link to="" class="kt-mycart__title">
+                    {{item.title}}
+                  </nuxt-link>
                   <span class="kt-mycart__desc">
-																Profile info, Timeline etc
-															</span>
+                    {{item.desc}}
+                  </span>
                   <div class="kt-mycart__action">
-                    <span class="kt-mycart__price">$ 450</span>
+                    <span class="kt-mycart__price">$ {{item.price}}</span>
                     <span class="kt-mycart__text">for</span>
-                    <span class="kt-mycart__quantity">7</span>
-                    <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                    <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
+                    <span class="kt-mycart__quantity">{{item.quantity}}</span>
+                    <span class="btn btn-label-success btn-icon">−</span>
+                    <span class="btn btn-label-success btn-icon">+</span>
                   </div>
                 </div>
-                <a href="#" class="kt-mycart__pic">
-                  <img src="assets/media/products/product9.jpg" title="">
-                </a>
+                <nuxt-link to="" class="kt-mycart__pic">
+                  <img :src="require('@/assets/media/products/' + item.image)" title="">
+                </nuxt-link>
+
               </div>
             </div>
-            <div class="kt-mycart__item">
-              <div class="kt-mycart__container">
-                <div class="kt-mycart__info">
-                  <a href="#" class="kt-mycart__title">
-                    Panasonic
-                  </a>
-                  <span class="kt-mycart__desc">
-																For PHoto & Others
-															</span>
-                  <div class="kt-mycart__action">
-                    <span class="kt-mycart__price">$ 329</span>
-                    <span class="kt-mycart__text">for</span>
-                    <span class="kt-mycart__quantity">1</span>
-                    <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                    <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                  </div>
-                </div>
-                <a href="#" class="kt-mycart__pic">
-                  <img src="assets/media/products/product13.jpg" title="">
-                </a>
-              </div>
-            </div>
-            <div class="kt-mycart__item">
-              <div class="kt-mycart__container">
-                <div class="kt-mycart__info">
-                  <a href="#" class="kt-mycart__title">
-                    Fujifilm
-                  </a>
-                  <span class="kt-mycart__desc">
-																Profile info, Timeline etc
-															</span>
-                  <div class="kt-mycart__action">
-                    <span class="kt-mycart__price">$ 520</span>
-                    <span class="kt-mycart__text">for</span>
-                    <span class="kt-mycart__quantity">6</span>
-                    <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                    <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                  </div>
-                </div>
-                <a href="#" class="kt-mycart__pic">
-                  <img src="assets/media/products/product16.jpg" title="">
-                </a>
-              </div>
-            </div>
-            <div class="kt-mycart__item">
-              <div class="kt-mycart__container">
-                <div class="kt-mycart__info">
-                  <a href="#" class="kt-mycart__title">
-                    Candy Machine
-                  </a>
-                  <span class="kt-mycart__desc">
-																For PHoto & Others
-															</span>
-                  <div class="kt-mycart__action">
-                    <span class="kt-mycart__price">$ 784</span>
-                    <span class="kt-mycart__text">for</span>
-                    <span class="kt-mycart__quantity">4</span>
-                    <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                    <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                  </div>
-                </div>
-                <a href="#" class="kt-mycart__pic">
-                  <img src="assets/media/products/product15.jpg" title="" alt="">
-                </a>
-              </div>
-            </div>
-          </div>
+          </perfect-scrollbar>
+
+
           <div class="kt-mycart__footer">
             <div class="kt-mycart__section">
               <div class="kt-mycart__subtitel">
@@ -114,6 +70,7 @@
                 <span>Taxes</span>
                 <span>Total</span>
               </div>
+
               <div class="kt-mycart__prices">
                 <span>$ 840.00</span>
                 <span>$ 72.00</span>
@@ -125,16 +82,71 @@
             </div>
           </div>
         </div>
-
-        <!-- end:: Mycart -->
       </form>
     </div>
   </div>
 </template>
 
 <script>
+    import ClickOutside from 'vue-click-outside'
+
     export default {
-        name: "cart"
+        name: "cart",
+        data () {
+          return {
+            bOpen: false,
+            dropdownFitPosition: 0,
+            items: [
+              {
+                title: 'Samsung',
+                desc: 'Profile info, Timeline etc',
+                price: 450,
+                quantity: 7,
+                image: 'product9.jpg'
+              },
+              {
+                title: 'Panasonic',
+                desc: 'For PHoto & Others',
+                price: 329,
+                quantity: 1,
+                image: 'product13.jpg'
+              },
+              {
+                title: 'Fujifilm',
+                desc: 'Profile info, Timeline etc',
+                price: 520,
+                quantity: 6,
+                image: 'product16.jpg'
+              },
+            ]
+          }
+        },
+        methods: {
+          toggle () {
+            this.bOpen = !this.bOpen
+          },
+          close () {
+            this.bOpen = false
+          },
+          handleWindowResize () {
+            let topbarWrapperRect = this.$refs.topbarWrapper.getBoundingClientRect()
+            this.dropdownFitPosition = -380 + topbarWrapperRect.width
+
+            let scrollContent = this.$refs.scrollContent.$el
+            scrollContent.style.height = (window.screen.width > 1024) ? scrollContent.dataset.height + 'px' :
+            searchWrascrollContentpper.dataset.mobileHeight + 'px'
+          }
+        },
+        directives: {
+          ClickOutside
+        },
+        mounted () {
+          this.handleWindowResize()
+          window.addEventListener("resize", this.handleWindowResize)
+        },
+        beforeDestroy () {
+          window.removeEventListener("resize", this.handleWindowResize)
+        }
     }
 </script>
 
